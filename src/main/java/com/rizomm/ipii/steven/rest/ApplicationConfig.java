5c1d622b980dd440b7317df926db278e1c66c9d1
StevenDieu <steven.dieu@decathlon.com>
2016-12-13 14:43:30 +0100

    [REST] Start rest request

---
+++ src/main/java/com/rizomm/ipii/steven/rest/ApplicationConfig.java
@@ -0,0 +1,44 @@
+package com.rizomm.ipii.steven.rest;
+
+import org.eclipse.persistence.jaxb.rs.MOXyJsonProvider;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("rest")
+public class ApplicationConfig extends Application {
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  private final Set<Class<?>> classes;
+
+  // ======================================
+  // =            Constructors            =
+  // ======================================
+
+  public ApplicationConfig() {
+    HashSet<Class<?>> c = new HashSet<>();
+    c.add(Category.class);
+    c.add(Order.class);
+    c.add(Product.class);
+
+    c.add(MOXyJsonProvider.class);
+
+    classes = Collections.unmodifiableSet(c);
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  @Override
+  public Set<Class<?>> getClasses() {
+    return classes;
+  }
+
+}
