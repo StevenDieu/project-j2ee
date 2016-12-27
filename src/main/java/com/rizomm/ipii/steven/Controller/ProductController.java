651b87567404794b8cbd71052431d6f920d12a3f
Steven Dieu <groover.Dieu@gmail.com>
2016-12-27 23:45:54 +0100

    [JSF] Pagination on collection

--- src/main/java/com/rizomm/ipii/steven/Controller/ProductController.java
+++ src/main/java/com/rizomm/ipii/steven/Controller/ProductController.java
@@ -1,13 +1,22 @@
 package com.rizomm.ipii.steven.Controller;
 
 import com.rizomm.ipii.steven.dao.ICategoryDao;
+import com.rizomm.ipii.steven.dao.IProductDao;
+import com.rizomm.ipii.steven.helper.Utils;
 import com.rizomm.ipii.steven.model.Category;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 @Named
 @RequestScoped
@@ -16,9 +25,17 @@ public class ProductController implements Serializable {
     @EJB
     private ICategoryDao CD;
 
+    @EJB
+    private IProductDao PD;
+
     private static final long serialVersionUID = 1L;
 
     public List<Category> findAllCategory(){
         return CD.findAllCategory();
+    }
+
+    public double getCountAllProduct() {
+        int countProduct = PD.countAllProduct();
+        return Math.ceil((double) countProduct / 9) ;
     }
 }
